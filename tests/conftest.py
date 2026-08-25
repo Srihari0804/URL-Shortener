fec95474d78f0ea49242204c3f636e7d73a7c1ac
Srihari <srihari7019320@gmail.com>
2026-08-25 13:13:21 +0530

    Removed lines that was wiping of tables and created new tables

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,9 +11,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 @pytest.fixture
-def session():
-    Base.metadata.drop_all(bind=engine)   # wipe tables
-    Base.metadata.create_all(bind=engine)  # create fresh tables
+def session(): #No need to erase the tables as we are using CI
     db = TestingSessionLocal()
     try:
         yield db
