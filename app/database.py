3c6c2c83534985c15dca553db3fffb7645ae428b
Srihari <srihari7019320@gmail.com>
2026-08-25 13:24:00 +0530

    Added Fallback Database Url

--- app/database.py
+++ app/database.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.environ['SQLALCHEMY_DATABASE_URL']
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL",
+                          os.getenv("TEST_DATABASE_URL") # fallback if production URL is not set
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, autocommit = False, bind=engine)
