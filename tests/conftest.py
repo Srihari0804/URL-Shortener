813e528d6ddac2c35837c1ae4121c85a1d1b9972
Srihari <srihari7019320@gmail.com>
2026-08-25 13:21:57 +0530

    Removed Base import

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from fastapi.testclient import TestClient
-from app.database import Base,get_db
+from app.database import get_db
 from app.main import app
 import os
 
